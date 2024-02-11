1c1be44da30471bef838afd4d8100e72bd1eb5f0
Arun <arundyp@gmail.com>
2024-02-11 13:59:17 +0530

    source code

---
+++ test.java
@@ -0,0 +1,13 @@
+class test{
+	public static void main(String args[]){
+		
+		System.out.println("Welcome to jenkins tutorial");
+		System.out.println("running program from jenkins");
+		
+	}
+
+
+
+
+
+}
